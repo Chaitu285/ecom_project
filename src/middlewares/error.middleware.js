b92f6464330cfd30f9301a94a78ae607dae789ef
Chaitu <abc@loyalty.in>
2025-08-28 17:54:51 +0530

    sdf

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -9,6 +9,8 @@ const errorHandler = (err, req, res, next) => {
         return res.status(400).json({
             message: 'Validation failed',
             errors: err.errors
+
+            //////////
         });
     }
 
