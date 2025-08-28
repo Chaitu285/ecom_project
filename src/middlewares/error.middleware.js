2b35d84ba8234ccd9d6c50032591598e6c556009
Chaitu <abc@loyalty.in>
2025-08-28 17:42:32 +0530

    helo

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -11,6 +11,9 @@ const errorHandler = (err, req, res, next) => {
             errors: err.errors
         });
     }
+
+
+    //Hello all
     const status = err.status || 500;
     const message = err.message || 'Internal Server Error';
     if (process.env.NODE_ENV !== 'production') console.error(err);
